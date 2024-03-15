95babd38ab73c68949480910e65dd686103f25c7
occurence <joycacao@gmail.com>
2024-03-16 01:00:17 +0800

    Create, insert, and drop user table

--- nextjs-dashboard/app/api/add_user/route.ts
+++ nextjs-dashboard/app/api/add_user/route.ts
@@ -5,10 +5,16 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const ID = searchParams.get('ID');
   const Employee_ID = searchParams.get('Employee_ID');
- 
+  const First_name = searchParams.get('First_name');
+  const Last_name = searchParams.get('Last_name');
+  const Email = searchParams.get('Email');
+  const Password = searchParams.get('Password');
+  const Image_URL = searchParams.get('Image_url');
+
   try {
-    if (!ID || !Employee_ID) throw new Error('Pet and owner names required');
-    await sql`INSERT INTO user_info (ID, Employee_ID, First_name, Last_name, Email) VALUES (${ID}, ${Employee_ID});`;
+    if (!ID || !Employee_ID || !First_name || !Last_name || !Email || !Password || !Image_URL) throw new Error('All details required');
+    await sql`INSERT INTO user_info (ID, Employee_ID, First_name, Last_name, Email, Password, Image_URL) VALUES (
+                                  ${ID}, ${Employee_ID}, ${First_name}, ${Last_name}, ${Email}, ${Password}, ${Image_URL});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
--- nextjs-dashboard/app/api/drop-table/route.ts
+++ nextjs-dashboard/app/api/drop-table/route.ts
@@ -4,14 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE IF NOT EXISTS "user_info" (
-        "id" serial PRIMARY KEY NOT NULL,
-        "employee_id" varchar(256),
-        "first_name" varchar(256),
-        "last_name" varchar(256),
-        "email" varchar(256),
-        "password" varchar(256),
-        "image_url" varchar(256),
+      await sql`DROP TABLE "user_info"
     );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
